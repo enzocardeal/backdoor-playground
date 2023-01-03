35427281c549f083b9aefcca431a68f41e882e8e
Enzo Neves <enzocardeal@usp.br>
2023-01-03 16:38:36 -0300

    feat(unit-test-init) initial config for junit test

---
+++ src/main/test/br/usp/pcs/control/UserTest.java
@@ -0,0 +1,11 @@
+package br.usp.pcs.control;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+    @Test
+    void test() {
+    	assertTrue(true);
+    }
+}
