5afa7fbabb3df6d2daafe26ecfba716ced9a7833
Enzo Neves <enzocardeal@usp.br>
2023-01-04 11:04:12 -0300

    feat(unit-test-init) test for existingAccounts and for unexistingAccounts

---
+++ src/test/java/br/usp/pcs/control/UserTest.java
@@ -0,0 +1,37 @@
+package br.usp.pcs.control;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static br.usp.pcs.control.User.getUser;
+
+class UserTest {
+	String user;
+	
+	@BeforeEach
+	void setup() {
+		user = null;
+	}
+	
+    @Test
+    void checkExistingAccounts() {
+    	user = getUser("user", "123456");
+    	assertEquals("user", user);
+    	
+    	user = getUser("admin", "a123456");
+    	assertEquals("admin", user);
+    }
+    
+    @Test
+    void checkUnexistingAccounts() {
+    	
+    	user = getUser("aba", "eba");
+    	assertEquals(null, user);
+    	
+    	user = getUser("eba", "eba");
+    	assertEquals(null, user);
+    	
+    }
+}
