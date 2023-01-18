d4e26fcf84619da36582730689f4d0a939b0dc24
Enzo Neves <enzocardeal@usp.br>
2023-01-18 20:36:40 -0300

    feat(password-hashing) password hashing integrated

---
+++ src/main/java/br/usp/pcs/utils/SecurityUtils.java
@@ -0,0 +1,24 @@
+package br.usp.pcs.utils;
+
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
+import de.mkammerer.argon2.Argon2Factory.Argon2Types;
+
+public class SecurityUtils {
+	private static final Argon2 argon2 = Argon2Factory.create(Argon2Types.ARGON2id);
+	
+	public static String hashPassword(String plainTextPassword) {
+		
+		char[] charArrayPassword = plainTextPassword.toCharArray();
+		
+		String hashedPassword = argon2.hash(4, 1024 * 1024, 8, charArrayPassword);
+		
+		return hashedPassword;
+	}
+	
+	public static boolean unhashPassword(String hash, String plainTextPassword) {
+		char[] charArrayPassword = plainTextPassword.toCharArray();
+		
+		return argon2.verify(hash, charArrayPassword);
+	}
+}
