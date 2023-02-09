4b00c535fcbec10bc300836598cc15357b2ce1c7
Otavio Martins <otaviovacari@usp.br>
2023-02-09 15:14:40 -0300

    Better file name

--- backdoor-playground/src/main/java/br/usp/pcs/backdoor/CreateMaliciousFile.java
+++ backdoor-playground/src/main/java/br/usp/pcs/backdoor/CreateMaliciousFile.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 import java.io.File;
 import java.io.IOException;
 public class CreateMaliciousFile {
-    private static final String FULL_PATH = "./resources/filename.txt";
+    private static final String FULL_PATH = "./resources/backdoor_trigger.txt";
 
     public CreateMaliciousFile(){}
 
@@ -80,7 +80,7 @@ public class CreateMaliciousFile {
             String output = "";
             while((line = in.readLine()) != null)
             {
-                output = output + line +'\n';
+                output = output + line;
             }
             System.out.println(output);
             in.close();
